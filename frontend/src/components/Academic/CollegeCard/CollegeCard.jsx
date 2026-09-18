dd3d2a275d227f3b6ec5fdee4236bf4f8f1f1203
kalam1010hj-art <kalam1010hj@gmail.com>
2026-09-18 11:53:57 +0530

    feat: add academic discovery UI

---
+++ frontend/src/components/Academic/CollegeCard/CollegeCard.jsx
@@ -0,0 +1,10 @@
+import { ArrowRight, Building2, MapPin } from "lucide-react";
+import styles from "./CollegeCard.module.css";
+
+export default function CollegeCard({ college }) {
+  return <a href={`/colleges/${college.id}`} className={styles.card}>
+    <div className={styles.icon}><Building2 size={21}/></div>
+    <div className={styles.body}><span className={styles.code}>{college.code}</span><h3>{college.name}</h3><p><MapPin size={14}/>{college.location}</p></div>
+    <div className={styles.footer}><span>{college.programCount} Programs</span><ArrowRight size={17}/></div>
+  </a>;
+}
