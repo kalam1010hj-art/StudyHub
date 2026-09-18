ed2a1e0d936bc2f933025ef0df57a9b1cb803e31
kalam1010hj-art <kalam1010hj@gmail.com>
2026-09-18 11:21:14 +0530

    Polish StudyHub homepage hero and structure

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,4 +1,3 @@
-import { useEffect } from "react";
 import Hero from "../components/Hero/Hero";
 import PlatformStats from "../components/PlatformStats/PlatformStats";
 import ProgramExplorer from "../components/ProgramExplorer/ProgramExplorer";
@@ -7,24 +6,20 @@ import HowItWorks from "../components/HowItWorks/HowItWorks";
 import AcademicDiscovery from "../components/AcademicDiscovery/AcademicDiscovery";
 import ContributorCTA from "../components/ContributorCTA/ContributorCTA";
 import FinalCTA from "../components/FinalCTA/FinalCTA";
-import Footer from "../components/Footer/Footer";
-function Home(){
-    console.log("Home component rendered")
-    useEffect(()=>{
-        console.log("home mounted")
-        return ()=> console.log("home unmounted")
-    })
-    return (
-        <div>
-       <Hero/>
-       <PlatformStats/>
-       <ProgramExplorer/>
-       <ResourceShowcase/>
-       <HowItWorks/>
-       <AcademicDiscovery/>
-       <ContributorCTA/>
-       <FinalCTA/>
-      
-       </div>
-    )
-} export default Home;
+
+function Home() {
+  return (
+    <main>
+      <Hero />
+      <PlatformStats />
+      <ProgramExplorer />
+      <ResourceShowcase />
+      <HowItWorks />
+      <AcademicDiscovery />
+      <ContributorCTA />
+      <FinalCTA />
+    </main>
+  );
+}
+
+export default Home;
