c2fed1336a1be3ebaa8033b10f5ba499d58e8a72
kalam1010hj-art <kalam1010hj@gmail.com>
2026-09-24 15:05:21 +0530

    Clean up account settings imports

--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -1,6 +1,5 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from django.contrib.auth import update_session_auth_hash
 from rest_framework.authtoken.models import Token
 from django.contrib.auth import authenticate
 from .serializers import LoginSerializer,RegisterSerializer
