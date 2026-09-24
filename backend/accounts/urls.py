66f438b1e11bed30287ac09262d59bd582084cfd
kalam1010hj-art <kalam1010hj@gmail.com>
2026-09-24 15:03:52 +0530

    Add account security routes

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -1,10 +1,21 @@
 from django.urls import path
-from .views import LoginView,RegisterView,PublicProfileView,UserDetailsView
+from .views import (
+    LoginView,
+    RegisterView,
+    PublicProfileView,
+    UserDetailsView,
+    ChangePasswordView,
+    DeleteAccountView,
+    LogoutView,
+)
 
 
 urlpatterns = [
     path("login",LoginView.as_view()),
     path("register",RegisterView.as_view()),
     path("profile",UserDetailsView.as_view()),
-    path("publicProfile",PublicProfileView.as_view())
+    path("publicProfile",PublicProfileView.as_view()),
+    path("change-password", ChangePasswordView.as_view()),
+    path("delete-account", DeleteAccountView.as_view()),
+    path("logout", LogoutView.as_view())
 ]
