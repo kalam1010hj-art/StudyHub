ea912f5a95dd759731afb85e320f0257b1b31a7a
kalam1010hj-art <kalam1010hj@gmail.com>
2026-09-24 15:28:24 +0530

    Load global responsive styles

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,7 @@
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
+import "./styles/global.css";
 import ScrollToTop from "./components/ScrollToTop/ScrollToTop.jsx";
 import AuthProvider from "./context/AuthProvider.jsx";
 
