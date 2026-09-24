a4abf2c6f7308cd1171c32aad8cf6866ae3c0f92
kalam1010hj-art <kalam1010hj@gmail.com>
2026-09-24 18:04:37 +0530

    Fix forgot password route build

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -14,7 +14,9 @@ import College from "./pages/Academic/College";
 import ResourceHub from "./pages/Academic/ResourceHub";
 import NotFound from "./pages/NotFound/NotFound";
 import Register from "./pages/Register/Register";
-import Login from "./components/Login/Login";\nimport ForgotPassword from "./pages/ForgotPassword/ForgotPassword";\nimport ResetPassword from "./pages/ResetPassword/ResetPassword";
+import Login from "./components/Login/Login";
+import ForgotPassword from "./pages/ForgotPassword/ForgotPassword";
+import ResetPassword from "./pages/ResetPassword/ResetPassword";
 import Profile from "./pages/Profile/Profile";
 import PublicProfile from "./pages/PublicProfile/PublicProfile";
 import UploadResource from "./pages/Academic/Upload";
@@ -62,7 +64,9 @@ function App(){
 
         {/* Authentication and user Profile Routes */}
         <Route path="/login" element={<Login/>}/>
-        <Route path="/register" element={<Register/>}/>\n        <Route path="/forgot-password" element={<ForgotPassword/>}/>\n        <Route path="/reset-password/:uid/:token" element={<ResetPassword/>}/>
+        <Route path="/register" element={<Register/>}/>
+        <Route path="/forgot-password" element={<ForgotPassword/>}/>
+        <Route path="/reset-password/:uid/:token" element={<ResetPassword/>}/>
 
         {/* protected Routes */}
         <Route element={<ProtectedRoute/>}>
