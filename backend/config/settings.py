bc8705be93f0daf24957e44bbc9aec5c88b5dc05
Abdul kalam <kalam1010hj@gmail.com>
2026-09-24 11:54:54 +0530

    deployment settings are configured

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/6.1/ref/settings/
 
 from pathlib import Path
 from dotenv import load_dotenv
+import dj_database_url
 
 import os
 
@@ -24,12 +25,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-%79cslv)g-+&59ibez$n__h_x%3f2&y)l9_xrw(w%$qz4*fv*='
+SECRET_KEY =   os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG")
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+      "localhost",
+    "127.0.0.1",
+]
 
 
 # Application definition
@@ -74,6 +78,7 @@ ROOT_URLCONF = 'config.urls'
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
+    "https://study-hub-gold-delta.vercel.app/"
 ]
 TEMPLATES = [
     {
@@ -97,15 +102,11 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/6.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": os.getenv("DB_NAME"),
-        "USER": os.getenv("DB_USER"),
-        "PASSWORD": os.getenv("DB_PASSWORD"),
-        "HOST": os.getenv("DB_HOST"),
-        "PORT": os.getenv("DB_PORT"),
-        
-    }
+     "default": dj_database_url.parse(
+        os.environ.get("DATABASE_URL"),
+         conn_max_age=600,
+        ssl_require=True,
+    )
 }
 
 
